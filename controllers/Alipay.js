4685716c39ca2345893a32be249d0be379a8201e
Weiqian-Ni-29 <weiqianni29@gmail.com>
2025-04-26 20:54:32 +0800

    update

--- controllers/Alipay.js
+++ controllers/Alipay.js
@@ -23,9 +23,8 @@ router.post('/payment/notify', async (req, res) => {
       throw new Error("支付宝通知验签失败");
     }
 
-    console.log("req.body" + req.body);
     // 2. 解析支付宝通知参数
-    const result = alipaySdk.decryptNotifyParams(req.body);
+    const result = req.body;
     console.log("支付宝通知参数:", result);
 
     // 3. 处理交易成功逻辑
